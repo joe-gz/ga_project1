ddfae47bc715522cfa41bd88b8221e102534df24
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-25 01:11:55 -0400

    Added picture timer functionality
    After a player clicks a second picture choice,
    they will both revert back to hidden after 2 seconds

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,67 @@
 // alert("hello")
-var player1 = "player 1";
-var player2 = "player 2";
-var clickCount = 0;
-indexArray = [];
+function startGame (){
 
-for (var i=0; i<=20; i++){
-  $( "main" ).append( "<div class = 'card'><img class = 'underCard' src='images/cage1.JPG'></div>" );
-  $(".underCard").hide();
+  var player1 = "player 1";
+  var player2 = "player 2";
+  var clickCount = 0;
+  var turnCount = 0;
+  indexArray = [];
+  imageArray = [];
+
+  for (var i=0; i<=3; i++){
+    $( "main" ).append( "<div class = 'row'></div>" );
+  };
+
+  for (var j=0; j<=4; j++){
+    $( ".row" ).append( "<div class = 'card'><img class = 'underCard' src='images/cage"+j+".jpg'></div>" );
+    $(".underCard").hide();
+    console.log(j);
+  };
+
+
+  $("button").eq(0).on("click", function () {
+    player1 = $("input[name='1']").val();
+    player2 = $("input[name='2']").val();
+    $("header div").text("Player turn: "+player1)
+    console.log(player1);
+    console.log(player2);
+  })
+
+  $("button").on("click",function() {
+    $(".card").css("background","blue");
+    $(".underCard").hide();
+    $("header div").text("Player turn: "+player1)
+    clickCount = 0;
+    indexArray=[];
+    // $(".win").remove();
+  })
+
+  $(".card").each(function (){
+    $(this).on("click",function(){
+      if (clickCount < 1){
+        indexArray[$(this).index()] = player1;
+        $(this).children(".underCard").show();
+        clickCount++
+        // checkForWinner();
+      } else {
+        turnCount++
+        indexArray[$(this).index()] = player1;
+        // $(this).children(".underCard").show();
+        $(this).children(".underCard").show().delay(1000).queue(function(n) {
+          $(".underCard").hide(); n();
+        });
+        clickCount = 0
+        // checkForWinner();
+        if (turnCount%2 === 0){
+          $("header div").text("Player turn: "+player1)
+        } else {
+          $("header div").text("Player turn: "+player2)
+        }
+      }
+    })
+  });
+
+
 };
 
-$("button").eq(0).on("click", function () {
-  player1 = $("input[name='1']").val();
-  player2 = $("input[name='2']").val();
-  $("header div").text("Player turn: "+player1)
-  console.log(player1);
-  console.log(player2);
-})
-
-$("button").on("click",function() {
-  $(".card").css("background","blue");
-  $(".underCard").hide();
-  clickCount = 0;
-  turnClick=0;
-  indexArray=[0,1,2,3,4,5,6,7,8];
-  $(".win").remove();
-})
-
-$(".card").each(function (){
-  $(this).on("click",function(){
-    // if (clickCount %2 === 0 ){
-      indexArray[$(this).index()] = player1;
-      $(this).children(".underCard").show().delay(3000).queue(function(n) {
-        $(this).hide(); n();
-      });
-      clickCount++
-      // checkForWinner();
-      $("header div").text("Player turn: ")
-  })
-})
-
-
-
-//
-// $(".card").each(function (){
-//   $(this).on("click",function(){
-//     if (clickCount %2 === 0 ){
-//       indexArray[$(this).index()] = player1;
-//       $(this).children(".underCard").show();
-//       setTimeout(function() {
-//         $(this).children(".underCard").hide(); }, 5000);
-//       });
-//       clickCount++
-//       // checkForWinner();
-//       $("header div").text("Player turn: "+player2)
-//
-//     } else {
-//       indexArray[$(this).index()] = player2;
-//       $(this).children(".underCard").hide();
-//       clickCount++
-//       // checkForWinner();
-//       $("header div").text("Player turn: "+player1)
-//     }
-//   })
-// })
+startGame();
